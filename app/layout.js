ab3df048ed3fb777a2fbf86dd693cc2350dd3fd0
Vera <vera.zabeida@gmail.com>
2024-06-20 14:20:23 -0400

    init

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import './ui/global.modules.css';
+
+export const metadata = {
+  title: 'Jiri Kilevnik',
+  description: 'Portfolio website of Jiri Kilevnik',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Jiri Kilevnik</title>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
