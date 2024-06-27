3894c0752e6a8898ecfb19fa7b36bcc500a9aaa3
Vera <vera.zabeida@gmail.com>
2024-06-27 10:56:01 -0400

    try parallax

--- app/page.jsx
+++ app/page.jsx
@@ -9,14 +9,35 @@ export default function HomePage() {
             <div id="content">
                 <section id="showreel">
                     <iframe
-                        src="https://www.youtube.com/embed/O4fJxENtxxc?playlist=O4fJxENtxxc&autoplay=1&rel=0"
+                        src="https://www.youtube.com/embed/O4fJxENtxxc?playlist=O4fJxENtxxc&rel=0"
                         frameborder="0"
                         allow="autoplay; encrypted-media"
                         allowfullscreen
                     />
                 </section>
-                <section id="about">
-                    <Image src="/images/throne.webp" layout="fill" />
+                <section id="about" className="bg-img" style={{ backgroundImage: "url('/images/throne.webp')" }}>
+                    <div className="caption">
+                        <p>
+                            {"I'm an award-winning visual effects supervisor " +
+                                "and compositing artist for over 16 years. "}
+                        </p>
+                        <p>
+                            {
+                                "I have worked on many projects including commercials, " +
+                                "tv series, feature films, VR projects and artificial intelligence."
+                            }
+                        </p>
+                        <p>
+                            <b>
+                                Professional experience:
+                            </b>
+
+                            {
+                                " Flame, Nuke, After Effects, " +
+                                "Unreal Engine, Maya, vfx and on-set supervision, compositing, " +
+                                "motion desing, artificial inteligence."}
+                        </p>
+                    </div>
                 </section>
             </div>
         </div >
