3c9d3a0f6e1204a2281ffbc98ad56190ed360aa3
Vera <vera.zabeida@gmail.com>
2024-07-11 13:09:41 -0400

    add css color variables

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,19 +1,20 @@
 import '/styles/header.modules.css';
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <div id="header" >
             <div className='left desktop'>
-                <a href="#">AI Projects</a> {"  "}
-                <a href="#">Advertising</a> {"  "}
-                <a href="#">Episodic</a> {"  "}
-                <a href="/about">About</a>
+                <Link href="#">AI Projects</Link> {"  "}
+                <Link href="#">Advertising</Link> {"  "}
+                <Link href="#">Episodic</Link> {"  "}
+                <Link href="/about">About</Link>
             </div>
-            <h1><a href="/" >Jiri Kilevnik</a ></h1>
+            <h1><Link href="/" >Jiri Kilevnik</Link></h1>
             <div className='right desktop'>
-                <a href='https://www.linkedin.com/in/jiri-kilevnik-48a0584b/' target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></a>{""}
-                <a href='https://www.imdb.com/name/nm8945630/?ref_=nv_sr_srsg_4' target="_blank"><img src="/images/imdb.svg" alt="Imdb" /></a>{""}
-                <a href='mailto:kilevnik@gmail.com' target="_blank"><img src="/images/envelope.svg" alt="Email" /></a>{""}
+                <Link href='https://www.linkedin.com/in/jiri-kilevnik-48a0584b/' target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></Link>{""}
+                <Link href='https://www.imdb.com/name/nm8945630/?ref_=nv_sr_srsg_4' target="_blank"><img src="/images/imdb.svg" alt="Imdb" /></Link>{""}
+                <Link href='mailto:kilevnik@gmail.com' target="_blank"><img src="/images/envelope.svg" alt="Email" /></Link>{""}
             </div>
         </div >
     );
