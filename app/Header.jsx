241f16e6ddfdd0118233315d3a651d1643291bca
Vera <vera.zabeida@gmail.com>
2024-07-23 20:08:08 -0400

    start adding content to mobile menu

--- app/Header.jsx
+++ app/Header.jsx
@@ -4,25 +4,62 @@ import { Cross2Icon, RowsIcon } from '@radix-ui/react-icons';
 import { usePathname } from 'next/navigation'
 import * as Dialog from '@radix-ui/react-dialog';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
 import styles from '/styles/header.module.css';
 
-export default function Header() {
+const SocialLinks = () => {
+    return (
+        <>
+            <Link href='https://www.linkedin.com/in/jiri-kilevnik-48a0584b/' target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></Link>{""}
+            <Link href='https://www.imdb.com/name/nm8945630/?ref_=nv_sr_srsg_4' target="_blank"><img src="/images/imdb.svg" alt="Imdb" /></Link>{""}
+            <Link href='mailto:kilevnik@gmail.com' target="_blank"><img src="/images/envelope.svg" alt="Email" /></Link>{""}
+        </>
+    )
+}
+
+const LinkWrapper = ({ children, isMobile }) => {
+    if (isMobile) {
+        return (
+            <Dialog.Close asChild>
+                {children}
+            </Dialog.Close>
+        )
+    }
+    return children
+}
+
+
+const NavigationLinks = ({ isMobile }) => {
     const pathname = usePathname();
 
     return (
+        <>
+            <LinkWrapper isMobile={isMobile}>
+                <Link className={pathname === '/ai-projects' ? styles.active : ''} href="/ai-projects">AI Projects</Link>
+            </LinkWrapper>
+            <LinkWrapper isMobile={isMobile}>
+                <Link className={pathname === '/advertising' ? styles.active : ''} href="/advertising">Advertising</Link>
+            </LinkWrapper>
+            <LinkWrapper isMobile={isMobile}>
+                <Link className={pathname === '/episodic' ? styles.active : ''} href="/episodic">Episodic</Link>
+            </LinkWrapper>
+            <LinkWrapper isMobile={isMobile}>
+                <Link className={pathname === '/about' ? styles.active : ''} href="/about">About</Link>
+            </LinkWrapper>
+        </>
+    )
+}
+
+export default function Header() {
+    return (
         <div className={styles.header} >
             <div className={`${styles.left} ${styles.desktop}`}>
-                <Link className={pathname === '/ai-projects' ? styles.active : ''} href="/ai-projects">AI Projects</Link> {"  "}
-                <Link className={pathname === '/advertising' ? styles.active : ''} href="/advertising">Advertising</Link> {"  "}
-                <Link className={pathname === '/episodic' ? styles.active : ''} href="/episodic">Episodic</Link> {"  "}
-                <Link className={pathname === '/about' ? styles.active : ''} href="/about">About</Link>
+                <NavigationLinks />
             </div>
             <h1 className={styles.title}><Link href="/" >Jiri Kilevnik</Link></h1>
             <div className={`${styles.right} ${styles.desktop}`}>
-                <Link href='https://www.linkedin.com/in/jiri-kilevnik-48a0584b/' target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></Link>{""}
-                <Link href='https://www.imdb.com/name/nm8945630/?ref_=nv_sr_srsg_4' target="_blank"><img src="/images/imdb.svg" alt="Imdb" /></Link>{""}
-                <Link href='mailto:kilevnik@gmail.com' target="_blank"><img src="/images/envelope.svg" alt="Email" /></Link>{""}
+                <SocialLinks />
             </div>
 
             <Dialog.Root >
@@ -35,12 +72,25 @@ export default function Header() {
                             <Dialog.Close className={`${styles.mobileIconContainer} ${styles.mobileAction} ${styles.closeButton}`} aria-label="Close">
                                 <Cross2Icon />
                             </Dialog.Close>
-                            Hey show me!!
+                            <div className={styles.mobileLinks}>
+                                <NavigationLinks isMobile />
+                                <div className={styles.socialMobileLinksContainer}>
+                                    <SocialLinks />
+                                </div>
+                            </div>
                         </Dialog.Content>
-
                     </Dialog.Portal>
                 </div>
             </Dialog.Root >
         </div >
     );
+}
+
+NavigationLinks.PropTypes = {
+    isMobile: PropTypes.bool,
+}
+
+LinkWrapper.PropTypes = {
+    isMobile: PropTypes.bool,
+    children: PropTypes.node.isRequired
 }
