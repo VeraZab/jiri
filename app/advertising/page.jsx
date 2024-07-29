b5a761628c6a78eebb59de1dcfaaf298f2b43e48
Vera <vera.zabeida@gmail.com>
2024-07-29 17:02:34 -0400

    start adding some images

--- app/advertising/page.jsx
+++ app/advertising/page.jsx
@@ -1,5 +1,18 @@
-export default function Advertising() {
+import PortfolioGrid from '../PortfolioGrid';
+import PortfolioImage from '../PortfolioImage';
+import PortfolioPageLayout from '../PortfolioPageLayout';
+
+export default function AdvertisingProjects() {
     return (
-        <div />
+        <PortfolioPageLayout>
+            <PortfolioGrid>
+                <PortfolioImage src="/images/coldplay.png" alt="Coldplay x BTS: My Universe" />
+                <PortfolioImage src="/images/sunday-night-football.jpg" alt="Sunday Night Football: Carrie Underwood" />
+                <PortfolioImage src="/images/slipknot.jpg" alt="Nike: The Footballverse campaign" />
+                <PortfolioImage src="/images/2k24.webp" alt="WWE 2K24 game cover features Cody Rhodes, Rhea Ripley and Bianca Belair" />
+                <PortfolioImage src="/images/footballverse.jpeg" alt="Nike: The Footballverse campaign" />
+
+            </PortfolioGrid>
+        </PortfolioPageLayout>
     );
 }
--- app/ai-projects/page.jsx
+++ app/ai-projects/page.jsx
@@ -1,22 +1,17 @@
-import styles from '/styles/aiprojects.module.css';
 import PortfolioGrid from '../PortfolioGrid';
 import PortfolioImage from '../PortfolioImage';
-import Section from '../Section';
+import PortfolioPageLayout from '../PortfolioPageLayout';
 
 export default function AIProjects() {
     return (
-        <div className={styles.container}>
+        <PortfolioPageLayout>
             <PortfolioGrid>
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
-                <PortfolioImage src="/images/throne.webp" alt="" />
+                <PortfolioImage src="/images/shangshi.webp" alt="shang-chi: the legend of the ten rings - movie poster" />
+                <PortfolioImage src="/images/weeknd.jpg" alt="spotify x the weeknd - alone with me interractive experience" />
+                <PortfolioImage src="/images/2k24.webp" alt="WWE 2K24 game cover features Cody Rhodes, Rhea Ripley and Bianca Belair" />
+                <PortfolioImage src="/images/footballverse.jpeg" alt="Nike: The Footballverse campaign" />
             </PortfolioGrid>
-        </div>
+        </PortfolioPageLayout>
     );
 }
 
