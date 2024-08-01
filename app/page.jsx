0efb250e5036aa540940d8d807abbefb33e4f727
Vera <vera.zabeida@gmail.com>
2024-07-31 20:14:36 -0400

    try adding dummy reel

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
-import PortfolioPageLayout from "./PortfolioPageLayout";
+import styles from '/styles/home.module.css';
+import ReelVideo from "./ReelVideo";
 
 
 export default function HomePage() {
     return (
-        <PortfolioPageLayout >REEL VIDEO</PortfolioPageLayout>
+        <div className={styles.container}>
+            <ReelVideo src="/videos/reel-placeholder.mp4" />
+        </div>
     );
 }
