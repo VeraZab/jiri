1b9edbeebcf1b12752798b512b6ab2f8e3806608
Vera <vera.zabeida@gmail.com>
2024-08-02 11:44:27 -0400

    some mobile menu adj

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Cross2Icon, RowsIcon } from '@radix-ui/react-icons';
+import { Cross2Icon, RowsIcon, EnvelopeClosedIcon } from '@radix-ui/react-icons';
 import { usePathname } from 'next/navigation'
 import * as Dialog from '@radix-ui/react-dialog';
 import Link from 'next/link';
@@ -10,11 +10,11 @@ import styles from '/styles/header.module.css';
 
 const SocialLinks = () => {
     return (
-        <>
+        <div className={styles.right}>
             <Link href='https://www.linkedin.com/in/jiri-kilevnik-48a0584b/' target="_blank"><img src="/images/linkedin.svg" alt="LinkedIn" /></Link>{""}
             <Link href='https://www.imdb.com/name/nm8945630/?ref_=nv_sr_srsg_4' target="_blank"><img src="/images/imdb.svg" alt="Imdb" /></Link>{""}
-            <Link href='mailto:kilevnik@gmail.com' target="_blank"><img src="/images/envelope.svg" alt="Email" /></Link>{""}
-        </>
+            <Link href='mailto:kilevnik@gmail.com' target="_blank"><EnvelopeClosedIcon /></Link>{""}
+        </div>
     )
 }
 
@@ -34,7 +34,7 @@ const NavigationLinks = ({ isMobile }) => {
     const pathname = usePathname();
 
     return (
-        <>
+        <div className={styles.left}>
             <LinkWrapper isMobile={isMobile}>
                 <Link className={pathname === '/ai-projects' ? styles.active : ''} href="/ai-projects">AI Projects</Link>
             </LinkWrapper>
@@ -50,18 +50,18 @@ const NavigationLinks = ({ isMobile }) => {
             <LinkWrapper isMobile={isMobile}>
                 <Link className={pathname === '/about' ? styles.active : ''} href="/about">About</Link>
             </LinkWrapper>
-        </>
+        </div>
     )
 }
 
 export default function Header() {
     return (
         <div className={styles.header} >
-            <div className={`${styles.left} ${styles.desktop}`}>
+            <div className={styles.desktop}>
                 <NavigationLinks />
             </div>
             <h1 className={styles.title}><Link href="/" >Jiri Kilevnik</Link></h1>
-            <div className={`${styles.right} ${styles.desktop}`}>
+            <div className={styles.desktop}>
                 <SocialLinks />
             </div>
 
