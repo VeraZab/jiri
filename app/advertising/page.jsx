b5b4f7267ff9a47d32c135b773a0acc06df801ab
Vera <vera.zabeida@gmail.com>
2024-08-04 17:06:48 -0400

    more images and make text more legible

--- app/advertising/page.jsx
+++ app/advertising/page.jsx
@@ -7,20 +7,16 @@ export default function AdvertisingProjects() {
         <PortfolioPageLayout>
             <PortfolioGrid>
                 <PortfolioImage
-                    src="/images/coldplay.png"
-                    alt="Coldplay x BTS: My Universe"
-                    text="Coldplay x BTS"
-                    externalUrl="https://www.rodeofx.com/news/rodeo-advertising-and-experiences-gets-mtv-vma-and-aeaf-nominations"
+                    src="/images/greatest-show.jpeg"
+                    alt="Sky Sports: Why so serious, Roy? | The Greatest Show On Earth"
+                    text="Sky Sports: The Greatest Show on Earth"
+                    externalUrl="https://www.framestore.com/work/greatest-show-earth"
                 />
                 <PortfolioImage
-                    src="/images/sunday-night-football.jpg"
-                    alt="Sunday Night Football: Carrie Underwood"
-                />
-                <PortfolioImage
-                    src="/images/slipknot.png"
-                    alt="Nike: The Footballverse campaign"
-                    text="Slipknot: Unsainted"
-                    externalUrl="https://www.rodeofx.com/project/slipknot-unsainted"
+                    src="/images/tag.png"
+                    alt="TAG HEUER : Mysterious night at the Manufacture"
+                    text="TAG HEUER x Monopoly"
+                    externalUrl="https://www.youtube.com/watch?v=rAb5tOs-KlY"
                 />
                 <PortfolioImage
                     src="/images/2k24.webp"
@@ -30,6 +26,27 @@ export default function AdvertisingProjects() {
                     src="/images/footballverse.jpeg"
                     alt="Nike: The Footballverse campaign"
                 />
+
+                <PortfolioImage
+                    src="/images/coldplay.png"
+                    alt="Coldplay x BTS: My Universe"
+                    text="Coldplay x BTS"
+                    externalUrl="https://www.rodeofx.com/news/rodeo-advertising-and-experiences-gets-mtv-vma-and-aeaf-nominations"
+                />
+                <PortfolioImage
+                    src="/images/sunday-night-football.jpg"
+                    alt="Sunday Night Football: Carrie Underwood"
+                    text="NBC: Sunday Night Football x Carrie Underwood"
+                    externalUrl="https://www.rodeofx.com/project/sunday-night-football-2019"
+                />
+                <PortfolioImage
+                    src="/images/slipknot.png"
+                    alt="Nike: The Footballverse campaign"
+                    text="Slipknot: Unsainted"
+                    externalUrl="https://www.rodeofx.com/project/slipknot-unsainted"
+                />
+
+
             </PortfolioGrid>
         </PortfolioPageLayout>
     );
