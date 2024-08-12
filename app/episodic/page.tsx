321af01390181b8a802c778b551858c601d98ddb
Vera <vera.zabeida@gmail.com>
2024-08-11 20:38:41 -0400

    add episodic & film

--- app/episodic/page.tsx
+++ app/episodic/page.tsx
@@ -20,6 +20,125 @@ export default function Episodic() {
                     text="Prime Video: Lord of The Rings"
                     externalUrl="https://www.youtube.com/watch?v=x8UAUAuKNcU"
                 />
+                <PortfolioItem
+                    type="image"
+                    src="/images/mankind.webp"
+                    alt="Apple TV: For All Mankind"
+                    text="Apple TV: For All Mankind"
+                    externalUrl="https://www.youtube.com/watch?v=M4EOW9oqZ4k"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/wanda.webp"
+                    alt="Disney: WandaVision"
+                    text="Disney: WandaVision"
+                    externalUrl="https://www.youtube.com/watch?v=UBhlqe2OTt4"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/loki.webp"
+                    alt="Disney: Loki"
+                    text="Disney: Loki"
+                    externalUrl="https://www.youtube.com/watch?v=nW948Va-l10"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/falcon.webp"
+                    alt="Disney: The Falcon and the Winter Soldier"
+                    text="Disney: The Falcon and the Winter Soldier"
+                    externalUrl="https://www.youtube.com/watch?v=IWBsDaFWyTE"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/stranger4.webp"
+                    alt="Netflix: Stranger Things 4"
+                    text="Netflix: Stranger Things 4"
+                    externalUrl="https://www.youtube.com/watch?v=sBEvEcpnG7k&t=1s"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/stranger.webp"
+                    alt="Netflix: Stranger Things 3"
+                    text="Netflix: Stranger Things 3"
+                    externalUrl="https://www.youtube.com/watch?v=ut_aTGcCVYg"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/GOT.webp"
+                    alt="HBO: Game of Thrones"
+                    text="HBO: Game of Thrones"
+                    externalUrl="https://www.youtube.com/watch?v=OJjW9N9BbLc"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/boys.webp"
+                    alt="Prime Video: The Boys"
+                    text="Prime Video: The Boys"
+                    externalUrl="https://www.youtube.com/watch?v=MN8fFM1ZdWo"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/dion.webp"
+                    alt="Netflix: Raising Dion"
+                    text="Netflix: Raising Dion"
+                    externalUrl="https://www.youtube.com/watch?v=Z6koPaImHzY"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/lovecraft.webp"
+                    alt="HBO: Lovecraft Country"
+                    text="HBO: Lovecraft Country"
+                    externalUrl="https://www.youtube.com/watch?v=eEvK1Zwi9XY"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/sandman.webp"
+                    alt="Netflix: The Sandman"
+                    text="Netflix: The Sandman"
+                    externalUrl="https://www.youtube.com/watch?v=ZJXamnPUm0I"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/iknow.webp"
+                    alt="HBO: I Know This Much is True"
+                    text="HBO: I Know This Much is True"
+                    externalUrl="https://www.youtube.com/watch?v=U4KBGydqlVk"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/terror.webp"
+                    alt="BBC: The Terror"
+                    text="BBC: The Terror"
+                    externalUrl="https://www.youtube.com/watch?v=dUZKjdewbZQ"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/genious.webp"
+                    alt="National Geographic: Genious"
+                    text="National Geographic: Genious"
+                    externalUrl="https://www.youtube.com/watch?v=SICLBlHizUY"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/spotless.webp"
+                    alt="Netflix: Spotless"
+                    text="Netflix: Spotless"
+                    externalUrl="https://www.youtube.com/watch?v=xQNZYYe2A3k"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/crossing.webp"
+                    alt="Prime Video: Crossing Lines"
+                    text="Prime Video: Crossing Lines"
+                    externalUrl="https://www.youtube.com/watch?v=z8TOK_OwjVk"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/knightfall.webp"
+                    alt="History Channel: Knightfall"
+                    text="History Channel: Knightfall"
+                    externalUrl="https://www.youtube.com/watch?v=F9dtepkeR0I"
+                />
             </PortfolioGrid>
         </PortfolioPageLayout>
     )
--- app/film/page.tsx
+++ app/film/page.tsx
@@ -9,22 +9,113 @@ export default function FilmProjects() {
                 <PortfolioItem
                     type="image"
                     src="/images/shangshi.webp"
-                    alt="shang-chi: the legend of the ten rings - movie poster"
+                    alt="Shang-Chi: The Legend of the Ten Rings"
+                    text="Shang-Chi: The Legend of the Ten Rings"
                 />
                 <PortfolioItem
                     type="image"
-                    src="/images/weeknd.jpg"
-                    alt="spotify x the weeknd - alone with me interractive experience"
+                    src="/images/borderlands.webp"
+                    alt="Borderlands"
+                    text="Borderlands"
+                    externalUrl="https://www.youtube.com/watch?v=Icnysn53neU"
                 />
                 <PortfolioItem
                     type="image"
-                    src="/images/2k24.webp"
-                    alt="WWE 2K24 game cover features Cody Rhodes, Rhea Ripley and Bianca Belair"
+                    src="/images/thefall.webp"
+                    alt="The Fall Guy"
+                    text="The Fall Guy"
+                    externalUrl="https://www.youtube.com/watch?v=j7jPnwVGdZ8"
                 />
                 <PortfolioItem
                     type="image"
-                    src="/images/footballverse.jpeg"
-                    alt="Nike: The Footballverse campaign"
+                    src="/images/conjuring.webp"
+                    alt="The Conjuring"
+                    text="The Conjuring"
+                    externalUrl="https://www.youtube.com/watch?v=bBRAseMeAIU&t=1s"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/bloodshot.webp"
+                    alt="Bloodshot"
+                    text="Bloodshot"
+                    externalUrl="https://www.youtube.com/watch?v=vOUVVDWdXbo"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/starwars.jpg"
+                    alt="Star Wars: The Last Jedi"
+                    text="Star Wars: The Last Jedi"
+                    externalUrl="https://www.youtube.com/watch?v=Q0CbN8sfihY"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/blade.webp"
+                    alt="Blade Runner 2049"
+                    text="Blade Runner 2049"
+                    externalUrl="https://www.youtube.com/watch?v=gCcx85zbxz4"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/crimes.webp"
+                    alt="The Crimes of Grindelwald"
+                    text="The Crimes of Grindelwald"
+                    externalUrl="https://www.youtube.com/watch?v=8bYBOVWLNIs&t=7s"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/super.webp"
+                    alt="Super Intelligence"
+                    text="Super Intelligence"
+                    externalUrl="https://www.youtube.com/watch?v=XN6JawOGvpM"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/jumanji.webp"
+                    alt="Jumanji: The Next Level"
+                    text="Jumanji: The Next Level"
+                    externalUrl="https://www.youtube.com/watch?v=rBxcF-r9Ibs"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/aquaman.webp"
+                    alt="Aquaman"
+                    text="Aquaman"
+                    externalUrl="https://www.youtube.com/watch?v=2wcj6SrX4zw"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/it.webp"
+                    alt="IT Ends"
+                    text="IT Ends"
+                    externalUrl="https://www.youtube.com/watch?v=xhJ5P7Up3jA"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/aeronauts.webp"
+                    alt="The Aeronauts"
+                    text="The Aeronauts"
+                    externalUrl="https://www.youtube.com/watch?v=Rm4VnwCtQO8&t=22s"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/crawl.webp"
+                    alt="Crawl"
+                    text="Crawl"
+                    externalUrl="https://www.youtube.com/watch?v=H6MLJG0RdDE"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/superdogs.webp"
+                    alt="Superpower Dogs"
+                    text="Superpower Dogs"
+                    externalUrl="https://www.youtube.com/watch?v=MmWhKcAcvNo"
+                />
+                <PortfolioItem
+                    type="image"
+                    src="/images/fallen.webp"
+                    alt="Star Wars: The Hunting and the Fallen"
+                    text="Star Wars: The Hunting and the Fallen"
+                    externalUrl="https://www.youtube.com/watch?v=k4WVOivkNTI"
                 />
             </PortfolioGrid>
         </PortfolioPageLayout>
