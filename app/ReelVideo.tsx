48351335c675cf9a06a0261616d9b736d85259d3
Vera <vera.zabeida@gmail.com>
2024-08-15 15:18:30 -0400

    remove sound button

--- app/ReelVideo.tsx
+++ app/ReelVideo.tsx
@@ -1,28 +1,22 @@
-import { forwardRef, ForwardedRef } from 'react'
+import { forwardRef } from 'react'
 
 import styles from '/styles/reelvideo.module.css'
 
-const ReelVideo = forwardRef(
-    (
-        { src, muted }: { src: string; muted: boolean },
-        ref: ForwardedRef<HTMLVideoElement>
-    ) => {
-        return (
-            <video
-                className={styles.video}
-                autoPlay
-                loop
-                playsInline
-                preload="auto"
-                tabIndex={-1}
-                muted={muted}
-                ref={ref}
-            >
-                <source src={src} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-        )
-    }
-)
+const ReelVideo = forwardRef(({ src }: { src: string }) => {
+    return (
+        <video
+            className={styles.video}
+            autoPlay
+            loop
+            playsInline
+            preload="auto"
+            tabIndex={-1}
+            muted
+        >
+            <source src={src} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+    )
+})
 
 export default ReelVideo
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
 'use client'
-
-import { useState, useRef } from 'react'
-import { SpeakerLoudIcon, SpeakerOffIcon } from '@radix-ui/react-icons'
-
 import ReelVideo from './ReelVideo'
 import styles from '/styles/home.module.css'
 
 export default function HomePage() {
-    const [muted, setMuted] = useState(true)
-    const videoRef = useRef<HTMLVideoElement>(null)
-
-    const handleUnmuteClick = () => {
-        setMuted((currentState) => !currentState)
-        if (videoRef.current) {
-            videoRef.current.muted = !muted
-        }
-    }
-
     return (
         <div className={styles.container}>
-            <ReelVideo src="/videos/reel.mp4" muted={muted} ref={videoRef} />
-            <button onClick={handleUnmuteClick} className={styles.unmute}>
-                {muted ? <SpeakerOffIcon /> : <SpeakerLoudIcon />}
-            </button>
+            <ReelVideo src="/videos/reel.mp4" />
         </div>
     )
 }
